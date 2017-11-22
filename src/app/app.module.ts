c75bd5613c2973d8e6c777e75e1e79e02ae77fa9
haisheng-lin <hlin86@asu.edu>
2017-11-22 11:42:37 -0700

    Single Page Application Part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,23 @@ import { MaterialModule } from '@angular/material';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 import 'hammerjs';
+
+/** ======================= Components ======================= */
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { DishDetailComponent } from './dish-detail/dish-detail.component';
-
-import { DishService } from './services/dish.service';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+
+/** ======================= Services ======================= */
+import { DishService } from './services/dish.service';
+import { PromotionService } from './services/promotion.service';
+
+/** ======================= Modules ======================= */
+import { AppRoutingModule } from './app-routing/app-routing.module';
 
 @NgModule({
   declarations: [
@@ -19,15 +29,19 @@ import { FooterComponent } from './footer/footer.component';
     MenuComponent,
     DishDetailComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MaterialModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    AppRoutingModule
   ],
-  providers: [ DishService ],
+  providers: [ DishService, PromotionService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/services/dish.service.ts
+++ src/app/services/dish.service.ts
@@ -11,4 +11,14 @@ export class DishService {
   getDishes(): Dish[] {
     return DISHES;
   }
+
+  getDish(id: number): Dish {
+    // filter() 返回一个数组，由于每个 Dish 的 id 只有一个，所以返回第一个元素
+    return DISHES.filter((dish) => (dish.id === id))[0];
+  }
+
+  getFeaturedDish(): Dish {
+    // 同理， featured 为 true 的只有一个
+    return DISHES.filter((dish) => (dish.featured))[0];
+  }
 }
